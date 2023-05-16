63443a7e3568217ad0c69383412a5e2319c53944
Ashwin <acolaco@uci.edu>
2023-05-16 18:40:38 +0000

    Adding performance compute stuff

--- transactionsim/src/main/java/com/yap/project/App.java
+++ transactionsim/src/main/java/com/yap/project/App.java
@@ -1,14 +1,17 @@
 package com.yap.project;
 
+import java.sql.Connection;
+
 /**
  * Hello world!
  *
  */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        TransactionSender t1 = new TransactionSender(5, "/home/yap/tippers/project1/MixedSQLCommand/low_concurrency/sample_10000.txt");
+        TransactionSender t1 = new TransactionSender(5, "/home/yap/tippers/project1/MixedSQLCommand/low_concurrency/sample_10000.txt",Connection.TRANSACTION_READ_COMMITTED);
         t1.start();
     }
 }
